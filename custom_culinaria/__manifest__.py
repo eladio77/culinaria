bb77007653c76928acc465560a1795ad6abab8bb
Eladio Garcia <eladio77@gmail.com>
2022-10-13 07:15:12 -0300

    campos personalizados

---
+++ custom_culinaria/__manifest__.py
@@ -0,0 +1,19 @@
+{ 
+    'name': 'Custom_culinaria',
+    'summary': "Personalizaciones generales de Odoo Culinaria",
+    'description': """
+    Manage Library
+    ===============
+    Description related to library.
+    """,
+    'author': "Eladio Garcia",
+    'website': "http://eladiogarcia.com",
+    'category': "sin categoría",
+    'version': '1.1',
+    'depends': ['base'],
+    'data': [
+        'views/as_res_partner.view.xml',
+    ],
+    
+    
+    }
---
+++ esqueleto_modulo (1)/__manifest__.py
@@ -0,0 +1,18 @@
+{ 
+    'name': 'My library',
+    'summary': "Manejar libros facilmente",
+    'description': """
+    Manage Library
+    ===============
+    Description related to library.
+    """,
+    'author': "Eladio Garcia",
+    'website': "http://eladiogarcia.com",
+    'category': "sin categoría",
+    'version': '1.1',
+    'depends': ['base'],
+    'data': ['views/views.xml'],
+    'demo': ['demo.xml'],
+    
+    
+    }
